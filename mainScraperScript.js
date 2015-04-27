78668c8fb03e46e3aeb95f9a4625ab0cc32d014b
johndrkurtcom <john@drkurt.com>
2015-04-26 21:39:02 -0700

    still tuning the scraper

--- mainScraperScript.js
+++ mainScraperScript.js
@@ -1,14 +1,26 @@
 var request = require('request');
 var cheerio = require('cheerio');
-var cities = [];
+var ap = [];
+
+function Apartment(location, link, price){
+	this.location = location,
+	this.link = link,
+	this.price = price
+}
 
 request('http://sfbay.craigslist.org/search/roo', function(error, response, html){
 	if(!error && response.statusCode == 200) {
 		var $ = cheerio.load(html);
-		$('p.row').each(function(i, element){
-			var city = $('p.row').find('span.pnr').find('small').text();
-			cities.push(city);	
+		$('p.row').each(function(){
+			var base = $(this).find('span.l2');
+			var city = base.find('small').text().trim() || null;
+			var link = base.parent().find('a').attr('href');
+			var price = base.find('span.price').text() || null;
+			var a = new Apartment(city, link, price);
+			ap.push(a);
 		});
 	}
-	console.log(cities);
+	//console.log(ap[0]);
 });
+
+console.log(ap[0]);
