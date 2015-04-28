5729d688ca253d7e051b0f8c326085e4c24c1406
johndrkurtcom <john@drkurt.com>
2015-04-27 20:52:54 -0700

    still tuningthe scraper and added list to store data

--- mainScraperScript.js
+++ mainScraperScript.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var cheerio = require('cheerio');
-var ap = [];
+var list = require('./list.js');
+var ap = new list.List();
 
 function Apartment(location, link, price){
 	this.location = location,
@@ -8,19 +9,21 @@ function Apartment(location, link, price){
 	this.price = price
 }
 
-request('http://sfbay.craigslist.org/search/roo', function(error, response, html){
-	if(!error && response.statusCode == 200) {
-		var $ = cheerio.load(html);
-		$('p.row').each(function(){
-			var base = $(this).find('span.l2');
-			var city = base.find('small').text().trim() || null;
-			var link = base.parent().find('a').attr('href');
-			var price = base.find('span.price').text() || null;
-			var a = new Apartment(city, link, price);
-			ap.push(a);
-		});
-	}
-	//console.log(ap[0]);
-});
+function getApartment(callback){
+	request('http://sfbay.craigslist.org/search/roo', function(error, response, html){
+		if(!error && response.statusCode == 200) {
+			var $ = cheerio.load(html);
+			$('p.row').each(function(){
+				var base = $(this).find('span.l2');
+				var city = base.find('small').text().trim() || null;
+				var link = base.parent().find('a').attr('href');
+				var price = base.find('span.price').text() || null;
+				var a = new Apartment(city, link, price);
+				ap.append(a);
+			});
+			callback(ap);
+		}
+	});
+}
 
-console.log(ap[0]);
+getApartment(function(list){console.log(list.toString())});
